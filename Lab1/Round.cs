5e81fef0832e8d608bfd3f7100e2aaf57b678f60
Nekit <nikstriker64@gmail.com>
2025-10-14 17:03:26 +0300

    Добавлен класс Round

---
+++ Lab1/Round.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using Players;
+
+namespace Rounds
+{
+    enum RoundStatus { NotStarted, InProgress, Finished }
+
+    class Round
+    {
+        private int RoundNumber { get; set; }
+        private RoundStatus Status { get; set; }
+        public Round(int number)
+        {
+            RoundNumber = number;
+            Status = RoundStatus.NotStarted;
+        }
+        public void Start()
+        {
+            Status = RoundStatus.InProgress;
+            Console.WriteLine($"\nРаунд {RoundNumber} начался!\n");
+        }
+        public void End()
+        {
+            Status = RoundStatus.Finished;
+            Console.WriteLine($"Раунд {RoundNumber} закончился!");
+        }
+        public class MVP
+        {
+            private Player Player { get; }
+            public MVP(Player player) => Player = player;
+            public void Show() => Console.WriteLine($"MVP раунда: {Player.Name} ({Player.Kills} убийств(-а))");
+        }
+    }
+}
