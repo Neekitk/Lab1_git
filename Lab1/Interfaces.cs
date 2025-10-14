c19951b202e2c7a48297ee68fbf8447134edd270
Nekit <nikstriker64@gmail.com>
2025-10-14 16:42:19 +0300

    Добавлен класс Interfaces

---
+++ Lab1/Interfaces.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using Players;
+
+namespace Interfaces
+{
+    interface IMovable
+    {
+        void Move();
+    }
+
+    interface IDamageable
+    {
+        void TakeDamage(int amount, Player attacker = null);
+    }
+
+    interface IShootable
+    {
+        void Shoot(Player target);
+    }
+}
