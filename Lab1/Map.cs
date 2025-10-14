58eca7fea77d9642dd5f730621133414a42d6a8c
Nekit <nikstriker64@gmail.com>
2025-10-14 16:47:27 +0300

    Добавлен класс Map

---
+++ Lab1/Map.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace Maps
+{
+    class Map
+    {
+        public string Name { get; set; }
+        public string Size { get; set; }
+        public string SpotA { get; set; }
+        public string SpotB { get; set; }
+        public void Load() => Console.WriteLine($"Загрузка карты {Name}...\n");
+        public void ShowInfo()
+        {
+            Console.WriteLine($"Карта: {Name} | Размер: {Size} | A: {SpotA}, B: {SpotB}");
+        }
+        public override string ToString() => $"{Name} ({Size})";
+    }
+}
