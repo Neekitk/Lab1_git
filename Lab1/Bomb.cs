d633fbba1aa894d71ec8676e92f7decec980972e
Nekit <nikstriker64@gmail.com>
2025-10-14 15:58:10 +0300

    Добавлен класс Bomb

---
+++ Lab1/Bomb.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using Exceptions;
+
+namespace Bombs
+{
+    class Bomb
+    {
+        public bool IsPlanted { get; private set; }
+        private int Timer { get; set; }
+        private string SpotPlanted { get; set; }
+        public void Plant(string spotPlanted)
+        {
+            if (IsPlanted) throw new BombAlreadyPlantedException();
+            IsPlanted = true;
+            SpotPlanted = spotPlanted;
+            Timer = 40;
+            Console.WriteLine($"Бомба установлена на {SpotPlanted}, таймер: {Timer}");
+        }
+        public void Defuse()
+        {
+            if (IsPlanted)
+            {
+                IsPlanted = false;
+                Console.WriteLine("Бомба успешно раздефьюжена");
+            }
+        }
+        public void Explode()
+        {
+            if (IsPlanted)
+            {
+                Console.WriteLine("Бомба взорвалась!");
+                IsPlanted = false;
+            }
+        }
+    }
+}
